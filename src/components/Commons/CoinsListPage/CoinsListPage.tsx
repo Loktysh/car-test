86ad342a68194e1bb48311be2e5b75b64300613f
Loktysh Alexandr <vebster24@mail.ru>
2022-08-03 20:52:30 +0300

    feat: implement coin add modal window

--- src/components/Commons/CoinsListPage/CoinsListPage.tsx
+++ src/components/Commons/CoinsListPage/CoinsListPage.tsx
@@ -21,7 +21,20 @@ function CoinsListPage() {
     setLoading(true);
     getCoins();
   }, [getCoins, searchParams]);
+  const ModalCoinAdd = (props: any) => {
+    return (
+      <div className='modal'>
+        <div className='modal__content'>
+          <p className='modal__text'></p>
+          <input type='number' name='' id='' />
+          <button>Submit</button>
+          <button onClick={() => props.closeHandler()}>Χ</button>
+        </div>
+      </div>
+    );
+  };
   const CoinCard = (props: any) => {
+    const [isModal, setModal] = useState(false);
     const { name, price, rank, volumeUsd24Hr, marketCapUsd } = {
       name: props.name,
       rank: props.rank,
@@ -33,13 +46,17 @@ function CoinsListPage() {
       marketCapUsd: parseInt(props.marketCapUsd),
     };
     return (
-      <div className='table__head table__row'>
-        <div className='table__item table__rank'>{rank}</div>
-        <div className='table__item table__name'>{name}</div>
-        <div className='table__item table__price'>{price}</div>
-        <div className='table__item table__volume'>{volumeUsd24Hr}</div>
-        <div className='table__item table__cap'>{marketCapUsd}</div>
-      </div>
+      <>
+        {isModal && <ModalCoinAdd closeHandler={() => setModal(!isModal)} />}
+        <div className='table__head table__row'>
+          <div className='table__item table__rank'>{rank}</div>
+          <div className='table__item table__name'>{name}</div>
+          <div className='table__item table__price'>{price}</div>
+          <div className='table__item table__volume'>{volumeUsd24Hr}</div>
+          <div className='table__item table__cap'>{marketCapUsd}</div>
+          <button onClick={() => setModal(!isModal)}>Add</button>
+        </div>
+      </>
     );
   };
   return (
